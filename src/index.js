ad766898eafc3298ac01f8ac1aa1e4b1eaa46f0e
Pink Blade <fenyaforever@gmail.com>
2019-03-19 02:06:11 +0200

    feat:add solution love-triangle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 /**
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
- */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+ */module.exports = function getLoveTrianglesCount(preferences = []) {
+	const arr = preferences;
+	let t = 0;
+	for (let n = 0; n < (arr.length); n++) {
+		let l1 = arr[n];
+		let l2 = arr[l1 - 1];
+		let l3 = arr[l2 - 1];
+		if ( (l1 != l2) && (l2 != l3) && ((n+1) == l3) ) { 
+			t+=1;
+			arr[n]=' ';
+		};
+	};
+	return t;
 };
